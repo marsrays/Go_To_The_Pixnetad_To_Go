b7df63c0e33d8e26e9c4a036992029840122c671
marsrays <marsrays@gmail.com>
2017-11-25 00:01:52 +0800

    Update GoToThePixnetadToGo.js
    Fix no jQuery will get error

--- GoToThePixnetadToGo.js
+++ GoToThePixnetadToGo.js
@@ -1,39 +1,48 @@
 (function(){
     var AdHunterTrigger = 0;
     var count = 0;
+    var removeNode = function (selector) {
+        Array.prototype.forEach.call( document.querySelectorAll(selector), function( node ) {
+            node.parentNode.removeChild( node );
+        });
+    }
     var pixnetAdHunter = function () {
-		if("mobile" !== pix.js_error_type){
-			clearInterval(AdHunterTrigger);
-			return;
-		}
+        if("mobile" !== pix.js_error_type){
+            clearInterval(AdHunterTrigger);
+            return;
+        }
 
-		if(0 < jQuery("div.article-body").size()){
-		/* 移除你不會有興趣的其他文章提示*/
-			jQuery("#recommend-tools").remove();
-		/* 移除全版廣告遮罩 &lt; - 這種廣告真的很機掰 */
-			jQuery("div.pop-ad-cover").remove();
-			jQuery("#scupio_interstitialad").remove();
-			jQuery("#scupio_interstitial").remove();
-		/* 移除文章上下的廣告*/
-			jQuery("div.header-ad").remove();
-		/* 移除手機留言區後難看的廣告欄位 */
-			jQuery("div.pix-related-post").remove();
-			jQuery("div.article-ad").remove();
-			jQuery("div.hot-articles").remove();
-		/* 移除訂房訂票的廣告 */
-			jQuery("div.octopus").remove();
-			jQuery("#hotels-searchbox").remove();
-		}
-		
-		/* 後置載入廣告要持續追殺 */
-		if(0 < jQuery("ins.pixnet-ad").size() && 0 < jQuery("div.article-body").size()){
-			jQuery("ins.pixnet-ad").parent().remove();
-		}
+        if(0 < document.querySelectorAll("div.article-body").length){
+        /*  痞客邦發不出錢的MIB廣告 */
+            removeNode("#ad-mib");
+            removeNode("#recommend-tools");
+        /* 移除全版廣告遮罩 &lt; - 這種廣告真的很機掰 */
+            removeNode("div.pop-ad-cover");
+            removeNode("#scupio_interstitialad");
+            removeNode("#scupio_interstitial");
+        /* 移除文章上下的廣告*/
+            removeNode("div.header-ad");
+        /* 你不可能有興趣的文章 */
+            removeNode("div.pix-related-post");
+        /* 移除手機留言區後難看的廣告欄位 */
+            removeNode("div.article-ad");
+            removeNode("div.hot-articles");
+        /* 移除訂房訂票的廣告 */
+            removeNode("div.octopus");
+            removeNode("#hotels-searchbox");
+        }
+        
+        /* 後置載入廣告要持續追殺 */
+        if(0 < document.querySelectorAll("ins.pixnet-ad").length && 0 < document.querySelectorAll("div.article-body").length){
+            Array.prototype.forEach.call( document.querySelectorAll("ins.pixnet-ad"), function( node ) {
+                node.parentNode.parentNode.removeChild(node.parentNode);
+            });
+        }
 
-		count++;
-		if (30 < count) {
-		    clearInterval(AdHunterTrigger);
-		}
+        count++;
+        if (30 < count) {
+            clearInterval(AdHunterTrigger);
+        }
     };
     pixnetAdHunter();
     AdHunterTrigger = setInterval(pixnetAdHunter, 300);
